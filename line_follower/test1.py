27b8ef4216bf8b772aef56d4a908e63210053df5
raspi lern <farizalarifin4777@gmail.com>
2021-10-23 17:48:25 +0000

    Finall Line Follower

--- line_follower/test1.py
+++ line_follower/test1.py
@@ -3,41 +3,26 @@ import time
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
 
-IO.setup(2,IO.IN) #GPIO 2 -> Left IR out
-IO.setup(3,IO.IN) #GPIO 3 -> Right IR out
+IO.setup(2,IO.IN) #GPIO 2 -> Kiri IR out
+IO.setup(3,IO.IN) #GPIO 3 -> Kanan IR out
 
-IO.setup(4,IO.OUT) #GPIO 4 -> Motor 1 terminal A
-IO.setup(14,IO.OUT) #GPIO 14 -> Motor 1 terminal B
-
-IO.setup(17,IO.OUT) #GPIO 17 -> Motor Left terminal A
-IO.setup(18,IO.OUT) #GPIO 18 -> Motor Left terminal B
+IO.setup(20,IO.OUT) #GPIO 4 -> Motor 1 terminal A
+IO.setup(21,IO.OUT) #GPIO 14 -> Motor 1 terminal B
 
 while 1:
 
     if(IO.input(2)==True and IO.input(3)==True): #both while move forward     
-        IO.output(4,True) #1A+
-        IO.output(14,False) #1B-
-
-        IO.output(17,True) #2A+
-        IO.output(18,False) #2B-
+        IO.output(20,True) #1A+
+        IO.output(21,True) #1B-
 
     elif(IO.input(2)==False and IO.input(3)==True): #turn right  
-        IO.output(4,True) #1A+
-        IO.output(14,True) #1B-
-
-        IO.output(17,True) #2A+
-        IO.output(18,False) #2B-
+        IO.output(20,True) #1A+
+        IO.output(21,False) #1B-
 
     elif(IO.input(2)==True and IO.input(3)==False): #turn left
-        IO.output(4,True) #1A+
-        IO.output(14,False) #1B-
-
-        IO.output(17,True) #2A+
-        IO.output(18,True) #2B-
+        IO.output(20,False) #1A+
+        IO.output(21,True) #1B-
 
     else:  #stay still
-        IO.output(4,True) #1A+
-        IO.output(14,True) #1B-
-
-        IO.output(17,True) #2A+
-        IO.output(18,True) #2B-
+        IO.output(20,False) #1A+
+        IO.output(21,False) #1B-
